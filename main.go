97ca0382a98b18b5459b50855ceaa66a16a6d460
Michał Łęcicki <mlecicki@gmail.com>
2019-03-29 00:14:25 +0100

    Return error status when something goes wrong

--- main.go
+++ main.go
@@ -14,14 +14,13 @@ func main() {
 	arguments := os.Args
 	if len(arguments) == 1 {
 		fmt.Println("Please provide a port number!")
-		return
+		os.Exit(1)
 	}
 
 	PORT := ":" + arguments[1]
 	l, err := net.Listen("tcp4", PORT)
 	if err != nil {
-		fmt.Println(err)
-		return
+		panic(err)
 	}
 	defer l.Close()
 
