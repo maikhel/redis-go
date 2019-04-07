4833f304fb6768dfa54ce8c936df9bc0b1e3ef47
Michał Łęcicki <mlecicki@gmail.com>
2019-04-07 13:44:57 +0200

    Refactor commands

--- commands.go
+++ commands.go
@@ -1,8 +1,48 @@
 package main
 
-func ping(input string) string {
-	if input != "" {
-		return input
+import (
+	"strings"
+)
+
+func (s *sessionHandler) authenticate(args []string) string {
+	if len(args) != 1 {
+		return "-ERR Wrong number of arguments"
 	}
-	return "PONG"
+	if s.auth {
+		return "-ERR Already authenticated."
+	}
+
+	if args[0] == cfg.DefaultPassword {
+		s.auth = true
+		return "+OK"
+	}
+	return "-ERR Invalid password"
+}
+
+func (s *sessionHandler) ping(args []string) string {
+	response := "PONG"
+	if len(args) > 0 {
+		response = strings.Join(args, " ")
+	}
+
+	return response
+}
+
+func (s *sessionHandler) get(args []string) string {
+	if len(args) != 1 {
+		return "-ERR Wrong number of arguments"
+	}
+	result, ok := s.store.Load(args[0])
+	if ok {
+		return result.(string)
+	}
+	return "(nil)"
+}
+
+func (s *sessionHandler) set(args []string) string {
+	if len(args) != 2 {
+		return "-ERR Wrong number of arguments"
+	}
+	s.store.Store(args[0], args[1])
+	return "+OK"
 }
--- server.go
+++ server.go
@@ -34,15 +34,15 @@ func (s *sessionHandler) handle() {
 		input := strings.TrimSpace(string(netData))
 
 		var result string
-		result = s.ExecCommand(input, s.store) + "\n"
+		result = s.ExecCommand(input) + "\n"
 
 		s.conn.Write([]byte(string(result)))
 	}
 }
 
-func (s *sessionHandler) ExecCommand(input string, store *sync.Map) string {
+func (s *sessionHandler) ExecCommand(input string) string {
 	args := strings.Split(input, " ")
-	cmd := args[0]
+	cmd := strings.ToLower(args[0])
 
 	if !s.auth && cmd != "auth" {
 		return "-NOAUTH Authentication required."
@@ -50,49 +50,13 @@ func (s *sessionHandler) ExecCommand(input string, store *sync.Map) string {
 
 	switch cmd {
 	case "auth":
-		{
-			if len(args) < 2 {
-				return "-ERR Too few arguments"
-			}
-			if s.auth {
-				return "-ERR alrady authenticated."
-			}
-
-			if args[1] == cfg.DefaultPassword {
-				s.auth = true
-				return "+OK"
-			}
-			return "-ERR invalid password"
-		}
+		return s.authenticate(args[1:])
 	case "ping":
-		{
-			var arg string
-			if len(input) > 5 {
-				arg = input[5:]
-			} else {
-				arg = ""
-			}
-			return ping(arg)
-		}
+		return s.ping(args[1:])
 	case "get":
-		{
-			if len(args) < 2 {
-				return "-ERR Too few arguments"
-			}
-			result, ok := store.Load(args[1])
-			if ok {
-				return result.(string)
-			}
-			return "(nil)"
-		}
+		return s.get(args[1:])
 	case "set":
-		{
-			if len(args) < 3 {
-				return "-ERR Too few arguments"
-			}
-			store.Store(args[1], args[2])
-			return "+OK"
-		}
+		return s.set(args[1:])
 	default:
 		return "COMMAND NOT FOUND"
 	}
