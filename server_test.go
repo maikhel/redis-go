f5a40add258ac8f930b02b994abfe38f9fa9df2a
Michał Łęcicki <mlecicki@gmail.com>
2019-04-09 22:57:08 +0200

    Add more tests for handler

--- server_test.go
+++ server_test.go
@@ -15,20 +15,19 @@ func TestExecCommand(t *testing.T) {
 	args := "set hello world"
 	assert.Equal(t, testHandler.ExecCommand(args), "-NOAUTH Authentication required.", "should return error unless user authenticated")
 
-	testHandler.auth = true
+	cfg.DefaultPassword = "pass"
+	args = "auth pass"
+	assert.Equal(t, testHandler.ExecCommand(args), "+OK", "should authenticate")
+
+	args = "ping hello"
+	assert.Equal(t, testHandler.ExecCommand(args), "hello", "Should ping")
 
 	args = "unknown command"
 	assert.Equal(t, testHandler.ExecCommand(args), "-ERR Command not found", "should return error if command not known")
 
-	// args = "auth"
-	// assert.Equal(t, testHandler.ExecCommand(args), "auth", "should invoke correct command")
-	//
-	// args = "set bob 3"
-	// assert.Equal(t, testHandler.ExecCommand(args), "set", "should invoke correct command")
-	//
-	// args = "get bob"
-	// assert.Equal(t, testHandler.ExecCommand(args), "get", "should invoke correct command")
-	//
-	// args = "ping"
-	// assert.Equal(t, testHandler.ExecCommand(args), "ping", "should invoke correct command")
+	args = "set bob 3"
+	assert.Equal(t, testHandler.ExecCommand(args), "+OK", "Should set value")
+
+	args = "get bob"
+	assert.Equal(t, testHandler.ExecCommand(args), "3", "Should get value")
 }
